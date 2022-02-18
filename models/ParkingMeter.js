5ecaf9cc41c2496fcf916e49c7cad2b17b915550
unknown <osskrjuarez@gmail.com>
2022-02-18 15:33:15 -0300

    Agregado middleware a server.js para parse y lectura de datos recibidos en el body

---
+++ models/ParkingMeter.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+const parkingMeterSchema = new Schema(
+  {
+  
+   
+   
+ 
+  
+    address: {
+      type: "string",
+      require: true,
+      trim: true,
+    },
+    available: {
+      type: "boolean",
+      default: false,
+    },
+    deleted: {
+      type: "boolean",
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionkey: false,
+  }
+);
+
+module.exports = model("User", userSchema);
